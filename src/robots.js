061a972f4634a16b744358054fad9b9a0a6430d5
hheli <hheli@mail.ru>
2018-08-12 22:26:15 +0300

    first comit (lock and load)

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+    {
+        id:1,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'Sincer@april.biz'
+    },
+    {
+        id:2,
+        name: 'Ervin Howell',
+        username: 'Antonette',
+        email: 'Shanna@melissa.tv'
+    },
+    {
+        id:3,
+        name: 'Clementine Bauch',
+        username: 'Samantha',
+        email: 'Nathan@yesenia.net'
+    },
+    {
+        id:4,
+        name: 'Dimitrios Mendeleeviy',
+        username: 'Dominic',
+        email: 'dom@shol.com'
+    },
+    {
+        id:5,
+        name: 'Paracetomol Ferein',
+        username: 'Tabletos',
+        email: 'tablet@gmail.com'
+    },
+    {
+        id:6,
+        name: 'Nail Foil',
+        username: 'NoFi',
+        email: 'd@sho.com'
+    },
+    {
+        id:7,
+        name: 'Kebordus Kvint',
+        username: 'Datey',
+        email: 'datey@mail.com'
+    },
+    {
+        id:8,
+        name: '1 1',
+        username: '1',
+        email: '1@1.1'
+    },
+    {
+        id:9,
+        name: 'Medv Muhameddin',
+        username: 'Medv',
+        email: '5675@list.com'
+    },
+    {
+        id:10,
+        name: 'Konstatis Habenkus',
+        username: 'Hokus',
+        email: 'khoc@gmail.com'
+    }
+]
